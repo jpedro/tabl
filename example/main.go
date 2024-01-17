1c74c59e796ecb1db2f9bd29e2349fcb827ecabc
Pedro Barbosa <jpedro.barbosa@gmail.com>
2024-01-17 06:33:37 +0100

    Fixed a bug in NoteLineCount... not seriously...

--- example/main.go
+++ example/main.go
@@ -9,7 +9,7 @@ func main() {
 
 	table.Add("KEY", "DESCRIPTION", "NUMBER_NUMBER_NUMBER", "ALMOST A NUMBER")
 	table.Add("Some metric", "a", "1", 1)
-	table.Add("This is a looooong value", "Some text", "1", 1.23)
+	table.Add("This is a looooong key", "Some text", "1", 1.23)
 	table.Add("key", "And now for something completely different", "\033[31;1m333\033[0m", "3a")
 
 	table.Print()

5e79cafd8377b6260bb4e0fd4385077fd4cb6321
Pedro Barbosa <jpedro.barbosa@gmail.com>
2024-01-17 16:57:13 +0100

    put code that worked where the code that didn't used to be

--- example/main.go
+++ example/main.go
@@ -10,7 +10,7 @@ func main() {
 	table.Add("KEY", "DESCRIPTION", "COLORED_NUMBERS", "ALMOST_A_NUMBER")
 	table.Add("Some metric", "", "1", 1)
 	table.Add("This is a looooong key", "Some text", "1", 1.23)
-	table.Add("Uh", "And now for something completely different", "\033[32;1m333\033[0m", "3")
+	table.Add("Uh", "Integer with color codes", "\033[32;1m333\033[0m", "3a")
 
 	table.Print()
 }
